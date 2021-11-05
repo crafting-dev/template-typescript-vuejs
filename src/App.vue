9f2b275d24d68ada52efa8ca3f93c1ee59bdb67e
i <i@alexandermaru.com>
2021-11-05 23:20:38 +0000

    Add a Ping component for templates to ping pong

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Ping />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Ping from './components/Ping.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Ping
   }
 })
 </script>
